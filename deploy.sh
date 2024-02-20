5eb25265885396c48890aeacbf9385626cd7dbd2
Delight-Fela-Steve <dfelastevegit@gmail.com>
2024-02-20 23:29:13 +0100

    checking directory

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ echo 'deploying postgres image..............'
 echo "checking the present working directory............"
 $GITHUB_WORKSPACE
 
-
 echo "checking the script present working directory............."
-nano "created a file called something.txt" something.txt
-pwd/"something.txt"
+echo "created a file called something.txt" > something.txt
+echo $PWD
+echo $PWD/something.txt
+echo "$PWD/something.txt"
+cat "$PWD/something.txt"
