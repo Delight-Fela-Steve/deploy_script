a44d1d300e9039f65991825a0ab7c1c303bd1753
Delight-Fela-Steve <dfelastevegit@gmail.com>
2024-02-20 22:55:12 +0100

    checking directory

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 echo 'deploying postgres image..............'
+
+PWD=${PWD}
+echo "checking the present working directory............"
+PWD
+
+
+echo "checking the script present working directory............."
+pwd
